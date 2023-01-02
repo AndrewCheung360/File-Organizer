da6884eb5e26317f469ca91943c209265060954f
DashingDolphin <andrewcheung360@gmail.com>
2023-01-01 23:27:26 -0500

    Updated file organizer

--- DownloadedFileOrganizer.py
+++ DownloadedFileOrganizer.py
@@ -3,5 +3,25 @@ import os
 import glob
 import csv
 
-def moveFiles():
-    
+def moveFiles(src,dest,folderName,keyword):
+    pattern = src + "\\*"+ keyword + "*"
+    destFolder = os.path.join(dest,folderName)
+    if(os.path.exists(destFolder) == False):
+        os.mkdir(destFolder)
+    for file in glob.iglob(pattern,recursive=True):
+        if(os.path.isfile(file)):
+            fileName = os.path.basename(file)
+            dest1=os.path.join(destFolder,fileName)
+            shutil.move(file,dest1)
+
+csvInput = input("Enter the path of the csv file: ")
+with open(csvInput) as csvFile:
+    csvReader = csv.reader(csvFile,delimiter=",")
+    lineCount = 0
+    for row in csvReader:
+        if lineCount == 0:
+            lineCount += 1
+        else:
+            moveFiles(row[0],row[1],row[2],row[3])
+            lineCount += 1
+
